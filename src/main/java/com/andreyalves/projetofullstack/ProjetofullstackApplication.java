a55cbdd2966557858ff0ba4fe22639c7e3b76082
Andrey Alves <andreyalvesgit@gmail.com>
2019-08-21 19:34:57 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/andreyalves/projetofullstack/ProjetofullstackApplication.java
+++ src/main/java/com/andreyalves/projetofullstack/ProjetofullstackApplication.java
@@ -13,6 +13,7 @@ import com.andreyalves.projetofullstack.domian.Cidade;
 import com.andreyalves.projetofullstack.domian.Cliente;
 import com.andreyalves.projetofullstack.domian.Endereco;
 import com.andreyalves.projetofullstack.domian.Estado;
+import com.andreyalves.projetofullstack.domian.ItemPedido;
 import com.andreyalves.projetofullstack.domian.Pagamento;
 import com.andreyalves.projetofullstack.domian.PagamentoComBoleto;
 import com.andreyalves.projetofullstack.domian.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.andreyalves.projetofullstack.repositories.CidadeRepository;
 import com.andreyalves.projetofullstack.repositories.ClienteRepository;
 import com.andreyalves.projetofullstack.repositories.EnderecoRepository;
 import com.andreyalves.projetofullstack.repositories.EstadoRepository;
+import com.andreyalves.projetofullstack.repositories.ItemPedidoRepository;
 import com.andreyalves.projetofullstack.repositories.PagamentoRepository;
 import com.andreyalves.projetofullstack.repositories.PedidoRepository;
 import com.andreyalves.projetofullstack.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class ProjetofullstackApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	
 
@@ -103,10 +107,11 @@ public class ProjetofullstackApplication implements CommandLineRunner {
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
+		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32") , cli1, e1);
-		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
 		
 		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pagto1);
@@ -119,6 +124,19 @@ public class ProjetofullstackApplication implements CommandLineRunner {
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 
 }
