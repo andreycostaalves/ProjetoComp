9ed68eb7905cd615ef4aa2f9f09dd464c5963374
Andrey Alves <andreyalvesgit@gmail.com>
2019-08-29 17:30:09 -0300

    DELETANDO uma Categoria - tratando resposta de ERRO

--- src/main/java/com/andreyalves/projetofullstack/services/CategoriaService.java
+++ src/main/java/com/andreyalves/projetofullstack/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.andreyalves.projetofullstack.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.andreyalves.projetofullstack.domian.Categoria;
 import com.andreyalves.projetofullstack.repositories.CategoriaRepository;
+import com.andreyalves.projetofullstack.services.exceptions.DataIntegrityException;
 import com.andreyalves.projetofullstack.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,6 +32,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try{
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel esxcluir uma categoria que possui produtos.");
+		}
 	}
 	
 }
